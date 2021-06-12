fc3469e24af1ee945057b6e16b454b563157cae2
David Valles <davidtjvalles@gmail.com>
2021-06-12 15:50:45 -0700

    Enable Chakra UI

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,5 +1,12 @@
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
