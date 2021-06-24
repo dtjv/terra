2230fa8bf4699fdff563a9812e886deb025a4154
David Valles <davidtjvalles@gmail.com>
2021-06-23 20:20:58 -0700

    Use one 1 page for now

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,14 @@
-function Home() {
-  return <div>Hello World</div>
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+import { Schedule } from '@/components/schedule'
+
+const Home = () => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Schedule />
+    </DndProvider>
+  )
 }
 
 export default Home
