a70bbeb7b0740f31744b3f4fab70ea0fcb25d6b9
David Valles <davidtjvalles@gmail.com>
2021-06-25 20:06:50 -0700

    Redesign types and data

---
+++ web/src/types/types.ts
@@ -0,0 +1,65 @@
+// ----------------------------------------------------------------------------
+//
+// data interfaces
+//
+// ----------------------------------------------------------------------------
+
+export interface ScheduleData {
+  scheduleStartTime: number
+  scheduleEndTime: number
+  timeIntervalInMinutes: number
+}
+
+export interface VehicleData {
+  id: string
+  vehicleName: string
+}
+
+export interface TimeData {
+  id: string
+  time: string
+}
+
+export interface TicketData {
+  id: string
+  scheduledDateTimeISO: string
+  vehicleId: string
+  durationInMinutes: number
+  timeRange?: string
+  scheduledStartTime?: string
+}
+
+export interface AppData {
+  schedule: ScheduleData
+  tickets: TicketData[]
+  vehicles: VehicleData[]
+}
+
+// ----------------------------------------------------------------------------
+//
+// ui interfaces
+//
+// ----------------------------------------------------------------------------
+
+export interface RowHeader {
+  display: string
+  data: TimeData
+}
+
+export interface ColHeader {
+  display: string
+  data: VehicleData | undefined
+}
+
+export interface DataCell {
+  rowIdx: number
+  colIdx: number
+  data: TicketData | undefined
+}
+
+export interface Grid {
+  rows: RowHeader[]
+  cols: ColHeader[]
+  cells: DataCell[][]
+  timeIntervalInMinutes: number
+}
