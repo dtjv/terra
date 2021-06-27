ee1767d558a7ffaa76c46b49d0ee9e9be7a45e9a
David Valles <davidtjvalles@gmail.com>
2021-06-27 15:57:56 -0700

    Refactor how schedule is rendered

--- web/src/components/schedule.tsx
+++ web/src/components/schedule.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import axios from 'axios'
 import { useQuery } from 'react-query'
-import { Flex, Grid, GridItem } from '@chakra-ui/react'
+import { Flex, Grid } from '@chakra-ui/react'
 
-import type { ScheduleMatrix, ColHeader } from '@/types/types'
 import { ScheduleColHeader } from '@/components/schedule-col-header'
+import { ScheduleRowHeader } from '@/components/schedule-row-header'
+import { ScheduleCell } from '@/components//schedule-cell'
+
+import type { Cell, ScheduleMatrix, ColHeader, RowHeader } from '@/types/types'
 
 // TODO: where do i put these? should they be configurable?
 const COL_WIDTH = '100px'
@@ -26,69 +29,47 @@ const useTickets = () => {
 export const Schedule: React.FC = () => {
   // TODO: do i need `isFetching`?
   const { data, isLoading } = useTickets()
-  const rowHeaders = data ? data.rowHeaders : []
-  const colHeaders = data ? data.colHeaders : []
-  const templateCols = `repeat(${colHeaders.length}, minmax(${COL_WIDTH}, 1fr))`
-  const templateRows = `repeat(${rowHeaders.length}, minmax(${ROW_HEIGHT}, 1fr))`
-
-  if (isLoading) return <div>loading...</div>
-
-  return (
-    <Flex p={8} align="center" justify="center">
-      <Grid templateRows={templateRows} templateColumns={templateCols}>
-        {colHeaders.map((colHeader: ColHeader) => {
-          return (
-            <GridItem
-              key={colHeader.data?.id}
-              borderBottomWidth="1px"
-              borderBottomColor="gray.600"
-            >
-              <ScheduleColHeader>{colHeader.display}</ScheduleColHeader>
-            </GridItem>
-          )
-        })}
-      </Grid>
-    </Flex>
-  )
-}
-/*
-import * as React from 'react'
-import { Flex, Grid, GridItem } from '@chakra-ui/react'
-
-import { Cell } from '@/components/cell'
-import { ColHeader } from '@/components/headers'
-
-const COL_WIDTH = '100px'
-const ROW_HEIGHT = '60px'
-
-export const Schedule = () => {
-  // TODO: needs to call api to get data.
-  const numRows = grid.grid.length
-  const numCols = grid.grid[0].length
-  const templateCols = `repeat(${numCols}, minmax(${COL_WIDTH}, 1fr))`
+  const cells: Cell[][] = data?.cells ?? []
+  const rowHeaders = data?.rowHeaders ?? []
+  const colHeaders = data?.colHeaders ?? []
+  const numRows = rowHeaders.length + 1
+  const numCols = colHeaders.length
   const templateRows = `repeat(${numRows}, minmax(${ROW_HEIGHT}, 1fr))`
+  const templateCols = `repeat(${numCols}, minmax(${COL_WIDTH}, 1fr))`
+
+  if (isLoading) {
+    return <div>render skeleton...</div>
+  }
 
   return (
     <Flex p={8} align="center" justify="center">
       <Grid templateRows={templateRows} templateColumns={templateCols}>
-        {grid.grid[0].map((column) => {
-          return (
-            <GridItem
-              key={`${column.rIdx}-${column.cIdx}`}
-              borderBottomWidth="1px"
-              borderBottomColor="gray.600"
-            >
-              <ColHeader>{column.data.display}</ColHeader>
-            </GridItem>
-          )
-        })}
-        {grid.grid.slice(1).map((row) => {
-          return row.map((cell) => (
-            <Cell key={`${cell.rIdx}-${cell.cIdx}`} cell={cell} grid={grid} />
-          ))
-        })}
+        {colHeaders.map((colHeader: ColHeader, colIdx) => (
+          <ScheduleColHeader key={`col-${colIdx}`} header={colHeader} />
+        ))}
+        {data
+          ? rowHeaders.map((rowHeader: RowHeader, rowIdx) => {
+              const row = [rowHeader, ...(cells[rowIdx] ?? [])]
+
+              return row.map((item, colIdx) => {
+                const key = `${rowIdx}-${colIdx}`
+
+                return colIdx === 0 ? (
+                  <ScheduleRowHeader
+                    key={key}
+                    header={item as RowHeader}
+                    rowIdx={rowIdx}
+                    colIdx={colIdx}
+                    numRows={numRows}
+                    numCols={numCols}
+                  />
+                ) : (
+                  <ScheduleCell key={key} cell={item as Cell} matrix={data} />
+                )
+              })
+            })
+          : null}
       </Grid>
     </Flex>
   )
 }
-*/
