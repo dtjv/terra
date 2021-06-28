d97cd3ecdd463ad70a4ba107e4c0295cba52661c
David Valles <davidtjvalles@gmail.com>
2021-06-28 10:32:13 -0700

    Fix drag handle and layering

--- web/src/components/ticket.tsx
+++ web/src/components/ticket.tsx
@@ -14,7 +14,7 @@ export const Ticket: React.FC<TicketProps> = ({
   ticket,
   timeIntervalInMinutes,
 }) => {
-  const [{ isDragging }, dragRef] = useDrag(
+  const [{ isDragging }, dragRef, dragPreviewRef] = useDrag(
     () => ({
       type: 'TICKET',
       item: ticket,
@@ -32,7 +32,7 @@ export const Ticket: React.FC<TicketProps> = ({
 
   return (
     <Box
-      ref={dragRef}
+      ref={dragPreviewRef}
       position="absolute"
       bg="papayawhip"
       w="85%"
@@ -40,7 +40,7 @@ export const Ticket: React.FC<TicketProps> = ({
       color="gray.900"
       borderRadius="4px"
       opacity={isDragging ? 0.5 : 1}
-      zIndex="10"
+      zIndex={isDragging ? -10 : 10}
     >
       <Box px={1} pt={1.5} lineHeight="1">
         <Flex
@@ -50,7 +50,9 @@ export const Ticket: React.FC<TicketProps> = ({
           fontWeight="bold"
         >
           {ticket.id}
-          <DragHandleIcon color="gray.900" cursor="grab" />
+          <div ref={dragRef}>
+            <DragHandleIcon color="gray.900" cursor="grab" />
+          </div>
         </Flex>
         <Box fontSize="xs" color="gray.700">
           {ticket.timeRange}
