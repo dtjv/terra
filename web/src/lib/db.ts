28d7894e54f8f581acf5b319d3f02af1999b9ab7
David Valles <davidtjvalles@gmail.com>
2021-06-28 15:22:37 -0700

    Add api to update a ticket

--- web/src/lib/db.ts
+++ web/src/lib/db.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import process from 'process'
 import * as fs from 'fs/promises'
 
-import type { AppData } from '@/types/types'
+import type { AppData, TicketData } from '@/types/types'
 
 export const getData = async (): Promise<AppData | undefined> => {
   try {
@@ -16,4 +16,37 @@ export const getData = async (): Promise<AppData | undefined> => {
   }
 
   return undefined
+}
+
+type UpdatedTicket = Pick<TicketData, 'id'> & Partial<TicketData>
+
+export const updateTicket = async (
+  updatedTicket: UpdatedTicket
+): Promise<TicketData | undefined> => {
+  const appData = await getData()
+
+  if (!appData) {
+    throw new Error('No data found')
+  }
+
+  let completeUpdatedTicket: TicketData | undefined = undefined
+
+  const updatedAppData = {
+    ...appData,
+    tickets: appData.tickets.map((ticket) => {
+      if (ticket.id === updatedTicket.id) {
+        completeUpdatedTicket = { ...ticket, ...updatedTicket }
+        return completeUpdatedTicket
+      }
+      return ticket
+    }),
+  }
+
+  await fs.writeFile(
+    path.resolve(__dirname, 'src/data/data.json'),
+    JSON.stringify(updatedAppData),
+    'utf8'
+  )
+
+  return completeUpdatedTicket
 }
---
+++ web/src/pages/api/tickets/index.ts
@@ -0,0 +1,23 @@
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+
+import type { TicketData } from '@/types/types'
+import { updateTicket } from '@/lib/db'
+
+//------------------------------------------------------------------------------
+// Handler for api calls to `/api/tickets`
+//------------------------------------------------------------------------------
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<TicketData | string>
+) => {
+  if (req.method === 'PATCH') {
+    const updatedTicket = await updateTicket(req.body.updatedTicket)
+
+    return updatedTicket
+      ? res.status(200).json(updatedTicket)
+      : res.status(204).send(`No ticket to update`)
+  }
+  return res.status(404).send(`Unsupported method: ${req.method}`)
+}
+
+export default handler
