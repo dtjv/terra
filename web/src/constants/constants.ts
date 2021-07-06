f28fe66fae582a736382990f2e9bb780e14342fe
David Valles <davidtjvalles@gmail.com>
2021-07-06 05:39:54 -0700

    Add ticket type and status enums

--- web/src/constants/constants.ts
+++ web/src/constants/constants.ts
@@ -1,3 +1,6 @@
+export const MIN_COL_WIDTH = 100
+export const MIN_ROW_HEIGHT = 60
+
 export enum CellKind {
   DATA_CELL = 'DATA_CELL',
   ROW_HEADER = 'ROW_HEADER',
@@ -8,5 +11,12 @@ export enum DragItem {
   TICKET = 'TICKET',
 }
 
-export const MIN_COL_WIDTH = 100
-export const MIN_ROW_HEIGHT = 60
+export enum TicketType {
+  DELIVERY = 'DELIVERY',
+  PICKUP = 'PICKUP',
+}
+
+export enum TicketStatus {
+  OPEN = 'OPEN',
+  CLOSED = 'CLOSED',
+}
