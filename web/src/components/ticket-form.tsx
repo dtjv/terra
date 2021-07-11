16194389c7ffb21b71cfa6047bab9095f3fb1f70
David Valles <davidtjvalles@gmail.com>
2021-07-11 09:30:25 -0700

    Simplify ticket schema and form

--- web/src/components/ticket-form.tsx
+++ web/src/components/ticket-form.tsx
@@ -13,25 +13,6 @@ import { TicketFormSchema } from '@/schemas/schemas'
 import type { TicketFormInputs } from '@/types/types'
 import { TicketType } from '@/constants/constants'
 
-// TODO: remove...
-let renderCount = 0
-
-// TODO: remove extras...
-const defaultValues = {
-  ticketType: TicketType.DELIVERY,
-  customerFirstName: 'hans',
-  customerLastName: 'gruber',
-  customerEmail: 'hans@gruber.com',
-  customerPhone: '(408) 280-9876',
-  deliveryAddress: {
-    street: '555 Market St.',
-    //  zip: '97301',
-  },
-  vehicleId: '102',
-  scheduledDateTimeISO: '1/1/2021',
-  durationInMinutes: 30,
-}
-
 //-----------------------------------------------------------------------------
 //
 // Ticket Form
@@ -46,9 +27,11 @@ export const TicketForm: React.FC = () => {
     handleSubmit,
     formState: { errors, isValid, isSubmitting },
   } = useForm<TicketFormInputs>({
-    defaultValues,
-    resolver: superstructResolver(TicketFormSchema, { coerce: true }),
     mode: 'onTouched',
+    defaultValues: {
+      durationInMinutes: 30,
+    },
+    resolver: superstructResolver(TicketFormSchema, { coerce: true }),
   })
 
   const watchDeliveryAddress = watch([
@@ -61,7 +44,7 @@ export const TicketForm: React.FC = () => {
       const { deliveryAddress, durationInMinutes } = getValues()
 
       // TODO
-      // replace w/ api
+      // call api
       // react-query should handle caching results (so api isn't called)
       const maps = (deliveryAddress: { zip: string; street: string }) => {
         switch (deliveryAddress.zip) {
@@ -75,7 +58,7 @@ export const TicketForm: React.FC = () => {
       }
       const estimatedDurationInMinutes = maps(deliveryAddress)
 
-      // this block prevents an infinite render loop by not updating form cache
+      // don't update form cache unnecessarily. prevents an infinite render loop
       if (estimatedDurationInMinutes !== durationInMinutes) {
         setValue('durationInMinutes', estimatedDurationInMinutes, {
           shouldValidate: true,
@@ -90,8 +73,7 @@ export const TicketForm: React.FC = () => {
   //
   //---------------------------------------------------------------------------
   const onSubmit: SubmitHandler<TicketFormInputs> = (data) => {
-    // TODO
-    // call api
+    // TODO: call api
     console.log('submitted', data)
   }
 
@@ -100,135 +82,65 @@ export const TicketForm: React.FC = () => {
   // Render
   //
   //---------------------------------------------------------------------------
-  renderCount++
-  console.log('renderCount', renderCount)
-
+  // TODO
+  // - make ticket type a radio button
+  // - improve error text
+  // - remove vehicle and schedule
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl isInvalid={!!errors.ticketType} isRequired>
-        <FormLabel htmlFor="ticketType">Ticket Type</FormLabel>
-        <Select
-          id="ticketType"
-          placeholder="Select option"
-          {...register('ticketType')}
-        >
+        <FormLabel htmlFor="ticketType">Ticket type</FormLabel>
+        <Select id="ticketType" {...register('ticketType')}>
           <option value={TicketType.DELIVERY}>Delivery</option>
           <option value={TicketType.PICKUP}>Pickup</option>
         </Select>
-        <FormErrorMessage>
-          {errors.ticketType && errors.ticketType.message}
-        </FormErrorMessage>
+        <FormErrorMessage>{errors?.ticketType?.message ?? ''}</FormErrorMessage>
       </FormControl>
 
-      <FormControl isInvalid={!!errors.customerFirstName} isRequired>
-        <FormLabel htmlFor="customerFirstName">First Name</FormLabel>
-        <Input
-          id="customerFirstName"
-          placeholder="Hans"
-          {...register('customerFirstName', {
-            required: `Customer's first name is required.`,
-          })}
-        />
+      <FormControl isInvalid={!!errors.customerName} isRequired>
+        <FormLabel htmlFor="customerFirstName">Name</FormLabel>
+        <Input id="customerName" {...register('customerName')} />
         <FormErrorMessage>
-          {errors.customerFirstName && errors.customerFirstName.message}
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl isInvalid={!!errors.customerLastName} isRequired>
-        <FormLabel htmlFor="customerLastName">Last Name</FormLabel>
-        <Input
-          id="customerLastName"
-          placeholder="Gruber"
-          {...register('customerLastName', {
-            required: `Customer's last name is required.`,
-          })}
-        />
-        <FormErrorMessage>
-          {errors.customerLastName && errors.customerLastName.message}
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl isInvalid={!!errors.customerEmail}>
-        <FormLabel htmlFor="customerEmail">Email</FormLabel>
-        <Input
-          id="customerEmail"
-          placeholder="hans_gruber@acme.com"
-          {...register('customerEmail')}
-        />
-        <FormErrorMessage>
-          {errors.customerEmail && errors.customerEmail.message}
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl isInvalid={!!errors.customerPhone} isRequired>
-        <FormLabel htmlFor="customerPhone">Phone</FormLabel>
-        <Input
-          id="customerPhone"
-          placeholder="(xxx) xxx-xxxx"
-          {...register('customerPhone')}
-        />
-        <FormErrorMessage>
-          {errors.customerPhone && errors.customerPhone.message}
+          {errors?.customerName?.message ?? ''}
         </FormErrorMessage>
       </FormControl>
 
       <FormControl isInvalid={!!errors.deliveryAddress?.street} isRequired>
-        <FormLabel htmlFor="deliveryAddress.street">Street Address</FormLabel>
+        <FormLabel htmlFor="deliveryAddress.street">Street address</FormLabel>
         <Input
           id="deliveryAddress.street"
-          placeholder="555 Market St."
           {...register('deliveryAddress.street')}
         />
         <FormErrorMessage>
-          {errors.deliveryAddress?.street &&
-            errors.deliveryAddress.street.message}
+          {errors?.deliveryAddress?.street?.message ?? ''}
         </FormErrorMessage>
       </FormControl>
 
       <FormControl isInvalid={!!errors.deliveryAddress?.zip} isRequired>
-        <FormLabel htmlFor="deliveryAddress.zip">Zip Code</FormLabel>
-        <Input
-          id="deliveryAddress.zip"
-          placeholder="97301"
-          {...register('deliveryAddress.zip')}
-        />
+        <FormLabel htmlFor="deliveryAddress.zip">ZIP / Postal</FormLabel>
+        <Input id="deliveryAddress.zip" {...register('deliveryAddress.zip')} />
         <FormErrorMessage>
-          {errors.deliveryAddress?.zip && errors.deliveryAddress.zip.message}
+          {errors?.deliveryAddress?.zip?.message ?? ''}
         </FormErrorMessage>
       </FormControl>
 
       <FormControl isInvalid={!!errors.vehicleId} isRequired>
         <FormLabel htmlFor="vehicleId">Vehicle</FormLabel>
-        <Input id="vehicleId" {...register('vehicleId')} />
-        <FormErrorMessage>
-          {errors.vehicleId && errors.vehicleId.message}
-        </FormErrorMessage>
+        <Select id="vehicleId" {...register('vehicleId')}>
+          <option value="102">Truck 102</option>
+          <option value="202">Truck 202</option>
+        </Select>
+        <FormErrorMessage>{errors?.vehicleId?.message ?? ''}</FormErrorMessage>
       </FormControl>
 
       <FormControl isInvalid={!!errors.scheduledDateTimeISO} isRequired>
-        <FormLabel htmlFor="scheduledDateTimeISO">Scheduled Date</FormLabel>
+        <FormLabel htmlFor="scheduledDateTimeISO">Schedule date</FormLabel>
         <Input
           id="scheduledDateTimeISO"
           {...register('scheduledDateTimeISO')}
         />
         <FormErrorMessage>
-          {errors.scheduledDateTimeISO && errors.scheduledDateTimeISO.message}
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl isInvalid={!!errors.numExtraPersons}>
-        <Select
-          id="numExtraPersons"
-          placeholder="How many helpers?"
-          {...register('numExtraPersons')}
-        >
-          <option value={0}>0</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-        </Select>
-        <FormErrorMessage>
-          {errors.numExtraPersons && errors.numExtraPersons.message}
+          {errors?.scheduledDateTimeISO?.message ?? ''}
         </FormErrorMessage>
       </FormControl>
 
--- web/src/schemas/schemas.ts
+++ web/src/schemas/schemas.ts
@@ -1,30 +1,8 @@
-import phone from 'phone'
-import { parseOneAddress } from 'email-addresses'
-import {
-  coerce,
-  define,
-  enums,
-  intersection,
-  number,
-  object,
-  optional,
-  size,
-  string,
-} from 'superstruct'
+import { coerce, enums, number, object, size, string } from 'superstruct'
 import oregon from '@/data/oregon.json'
 import { TicketType } from '@/constants/constants'
 
 export const IntSchema = coerce(number(), string(), (value) => parseInt(value))
-
-export const EmailSchema = define(
-  'EmailSchema',
-  (value) => !!parseOneAddress(value as string)
-)
-
-export const PhoneSchema = define(
-  'PhoneSchema',
-  (value) => !!phone(value as string).length
-)
 
 export const AddressSchema = object({
   street: size(string(), 2, 40),
@@ -33,16 +11,9 @@ export const AddressSchema = object({
 
 export const TicketFormSchema = object({
   ticketType: enums([TicketType.PICKUP, TicketType.DELIVERY]),
-  customerFirstName: size(string(), 2, 40),
-  customerLastName: size(string(), 2, 40),
-  customerEmail: intersection([string(), EmailSchema]),
-  customerPhone: PhoneSchema,
+  customerName: size(string(), 2, 40),
   deliveryAddress: AddressSchema,
-  //  orderId: optional(string()),
-  //  products: array(ProductSchema),
   vehicleId: string(),
   scheduledDateTimeISO: string(),
   durationInMinutes: IntSchema,
-  numExtraPersons: IntSchema,
-  notes: optional(string()),
 })
