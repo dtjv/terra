94628bf2ffd3e82ace0ff5362fef5848cffd94eb
David Valles <davidtjvalles@gmail.com>
2021-07-17 09:14:53 -0700

    Move schedule config to constants

--- web/src/constants/constants.ts
+++ web/src/constants/constants.ts
@@ -1,3 +1,7 @@
+export const SCHEDULE_START_HOUR = 8 // 24-hr clock
+export const SCHEDULE_END_HOUR = 18 // 24-hr clock
+export const SCHEDULE_TIME_BLOCK_IN_MINUTES = 30
+
 export const MIN_COL_WIDTH = 100
 export const MIN_ROW_HEIGHT = 60
 
