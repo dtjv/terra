828697deab863cc206451480e0aa8c725e2957f3
David Valles <davidtjvalles@gmail.com>
2021-07-21 12:16:57 -0700

    Add type specific utils

---
+++ web/src/types/utils.ts
@@ -0,0 +1,39 @@
+import type {
+  Ticket,
+  Vehicle,
+  TicketDocument,
+  VehicleDocument,
+} from '@/types/types'
+
+export const toVehicle = (vehicleDoc: VehicleDocument): Vehicle => {
+  const { id, key, name } = vehicleDoc
+  return { id, key, name }
+}
+
+export const toTicket = (ticketDoc: TicketDocument): Ticket => {
+  const {
+    id,
+    ticketKind,
+    customerName,
+    destinationAddress,
+    vehicleKey,
+    scheduledAt,
+    durationInMinutes,
+    vehicleDoc,
+    ticketRange,
+    scheduledStartTime,
+  } = ticketDoc
+
+  return {
+    id,
+    ticketKind,
+    customerName,
+    destinationAddress,
+    vehicleKey,
+    scheduledAt,
+    durationInMinutes,
+    vehicleDoc,
+    ticketRange,
+    scheduledStartTime,
+  }
+}
