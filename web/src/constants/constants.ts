c5aa1b460fd7afb6100c2e274ec95435391a4acb
David Valles <davidtjvalles@gmail.com>
2021-07-21 12:16:32 -0700

    Move enums under types

--- web/src/constants/constants.ts
+++ web/src/constants/constants.ts
@@ -1,26 +1,3 @@
-export const SCHEDULE_START_HOUR = 8 // 24-hr clock
-export const SCHEDULE_END_HOUR = 18 // 24-hr clock
-export const SCHEDULE_TIME_BLOCK_IN_MINUTES = 30
-
-export const MIN_COL_WIDTH = 100
-export const MIN_ROW_HEIGHT = 60
-
-export enum CellKind {
-  DATA_CELL = 'DATA_CELL',
-  ROW_HEADER = 'ROW_HEADER',
-  COL_HEADER = 'COL_HEADER',
-}
-
-export enum DragItem {
-  TICKET = 'TICKET',
-}
-
-export enum TicketKind {
-  DELIVERY = 'DELIVERY',
-  PICKUP = 'PICKUP',
-}
-
-export enum TicketStatus {
-  OPEN = 'OPEN',
-  CLOSED = 'CLOSED',
-}
+// css grid template settings
+export const MIN_ROW_HEIGHT = 100
+export const MIN_COL_WIDTH = 60
---
+++ web/src/types/enums.ts
@@ -0,0 +1,19 @@
+export enum CellKind {
+  DATA_CELL = 'DATA_CELL',
+  ROW_HEADER = 'ROW_HEADER',
+  COL_HEADER = 'COL_HEADER',
+}
+
+export enum DragItem {
+  TICKET = 'TICKET',
+}
+
+export enum TicketKind {
+  PICKUP = 'PICKUP',
+  DELIVERY = 'DELIVERY',
+}
+
+export enum TicketStatus {
+  OPEN = 'OPEN',
+  CLOSED = 'CLOSED',
+}
