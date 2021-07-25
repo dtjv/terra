50861adeb8b4d461150d61e1f2d3406c50223c63
David Valles <davidtjvalles@gmail.com>
2021-07-24 19:53:01 -0700

    Make use of field components

--- web/src/components/ticket/create-form.tsx
+++ web/src/components/ticket/create-form.tsx
@@ -9,6 +9,8 @@ import {
   FormControl,
   FormErrorMessage,
 } from '@chakra-ui/react'
+import { ScheduleAtISO } from '@/components/ticket/scheduled-at-iso'
+import { DestinationAddress } from '@/components/ticket/destination-address'
 import { superstructResolver } from '@hookform/resolvers/superstruct'
 import { TicketFormSchema } from '@/schemas/schemas'
 import { TicketKind } from '@/types/enums'
@@ -19,9 +21,6 @@ export const CreateTicketForm: React.FC = () => {
     mode: 'onTouched',
     defaultValues: {
       ticketKind: TicketKind.DELIVERY,
-      // TODO
-      // - try the demo form w/o this set.
-      vehicleKey: '102',
       scheduledAtISO: '',
       durationInMinutes: 30,
     },
@@ -68,39 +67,23 @@ export const CreateTicketForm: React.FC = () => {
         </FormErrorMessage>
       </FormControl>
 
-      <FormControl isInvalid={!!errors.destinationAddress?.street} isRequired>
-        <FormLabel htmlFor="destinationAddress.street">
-          Street address
-        </FormLabel>
-        <Input
-          id="destinationAddress.street"
-          {...register('destinationAddress.street')}
-        />
-        <FormErrorMessage>
-          {errors?.destinationAddress?.street?.message ?? ''}
-        </FormErrorMessage>
-      </FormControl>
+      <DestinationAddress {...form} />
 
-      <FormControl isInvalid={!!errors.destinationAddress?.zip} isRequired>
-        <FormLabel htmlFor="destinationAddress.zip">ZIP / Postal</FormLabel>
-        <Input
-          id="destinationAddress.zip"
-          {...register('destinationAddress.zip')}
-        />
-        <FormErrorMessage>
-          {errors?.destinationAddress?.zip?.message ?? ''}
-        </FormErrorMessage>
-      </FormControl>
-
-      {/*TODO: pull vehicle choices from env... i think.*/}
+      {/*TODO: pull vehicle choices from db*/}
       <FormControl isInvalid={!!errors.vehicleKey} isRequired>
         <FormLabel htmlFor="vehicleKey">Vehicle</FormLabel>
-        <Select id="vehicleKey" {...register('vehicleKey')}>
+        <Select
+          id="vehicleKey"
+          placeholder="Select a vehicle"
+          {...register('vehicleKey')}
+        >
           <option value="102">Truck 102</option>
           <option value="202">Truck 202</option>
         </Select>
         <FormErrorMessage>{errors?.vehicleKey?.message ?? ''}</FormErrorMessage>
       </FormControl>
+
+      <ScheduleAtISO {...form} />
 
       {/*TODO: test enable/disable on demo*/}
       <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
