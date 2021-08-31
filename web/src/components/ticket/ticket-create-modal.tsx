a1c88e463fa67b36f0b19750efedabc10b313997
David Valles <davidtjvalles@gmail.com>
2021-08-30 19:01:24 -0700

    Render modal with data. [wip]

--- web/src/components/ticket/ticket-create-modal.tsx
+++ web/src/components/ticket/ticket-create-modal.tsx
@@ -1,3 +1,5 @@
+import { groupBy } from 'lodash'
+import { format } from 'date-fns'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import {
   SimpleGrid,
@@ -9,24 +11,204 @@ import {
   HStack,
   Heading,
   Input,
-  Button,
-  Select,
+  //  Button,
+  //  Select,
   FormLabel,
   FormControl,
-  FormErrorMessage,
+  //  FormErrorMessage,
   Modal,
   ModalOverlay,
   ModalHeader,
   ModalBody,
   ModalContent,
   ModalCloseButton,
+  useRadio,
+  useRadioGroup,
 } from '@chakra-ui/react'
+import type { UseRadioProps } from '@chakra-ui/react'
 import { superstructResolver } from '@hookform/resolvers/superstruct'
 import type { UseFormReturn } from 'react-hook-form'
-import { ScheduleAt, DestinationAddress } from '@/components/ticket'
+//import { ScheduleAt, DestinationAddress } from '@/components/ticket'
 import { TicketFormSchema } from '@/schemas'
 import { TicketKind } from '@/types/enums'
 import type { TicketInput } from '@/types/types'
+
+// TODO: remove
+import { combineDateTime } from '@/lib/utils'
+
+interface AvailableSlot {
+  key: number
+  vehicleKey: string
+  vehicleName: string
+  scheduledAt: Date
+  scheduledTime: string
+  scheduledAtFull: Date
+}
+
+// TODO remove
+const slots: AvailableSlot[] = [
+  {
+    key: 1,
+    vehicleKey: '102',
+    vehicleName: 'Truck 102',
+    scheduledAt: new Date('2021-8-19'),
+    scheduledTime: '08:00:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-19'), '08:00:00.000'),
+  },
+  {
+    key: 2,
+    vehicleKey: '102',
+    vehicleName: 'Truck 102',
+    scheduledAt: new Date('2021-8-19'),
+    scheduledTime: '09:00:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-19'), '09:00:00.000'),
+  },
+  {
+    key: 3,
+    vehicleKey: '202',
+    vehicleName: 'Truck 202',
+    scheduledAt: new Date('2021-8-19'),
+    scheduledTime: '08:30:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-19'), '08:30:00.000'),
+  },
+  {
+    key: 4,
+    vehicleKey: '202',
+    vehicleName: 'Truck 202',
+    scheduledAt: new Date('2021-8-19'),
+    scheduledTime: '13:30:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-19'), '13:30:00.000'),
+  },
+  {
+    key: 5,
+    vehicleKey: '202',
+    vehicleName: 'Truck 202',
+    scheduledAt: new Date('2021-8-20'),
+    scheduledTime: '08:00:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-20'), '08:00:00.000'),
+  },
+  {
+    key: 6,
+    vehicleKey: '302',
+    vehicleName: 'Truck 302',
+    scheduledAt: new Date('2021-8-20'),
+    scheduledTime: '10:30:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-20'), '10:30:00.000'),
+  },
+  {
+    key: 7,
+    vehicleKey: '302',
+    vehicleName: 'Truck 302',
+    scheduledAt: new Date('2021-8-20'),
+    scheduledTime: '11:00:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-20'), '11:00:00.000'),
+  },
+  {
+    key: 8,
+    vehicleKey: '302',
+    vehicleName: 'Truck 302',
+    scheduledAt: new Date('2021-8-20'),
+    scheduledTime: '15:00:00.000',
+    scheduledAtFull: combineDateTime(new Date('2021-8-20'), '15:00:00.000'),
+  },
+]
+
+const TimeSlot = (props: UseRadioProps & { children?: React.ReactNode }) => {
+  const { getInputProps, getCheckboxProps } = useRadio(props)
+  const input = getInputProps()
+  const checkbox = getCheckboxProps()
+
+  return (
+    <Box as="label" d="block">
+      <input {...input} />
+      <Flex
+        {...checkbox}
+        p={2}
+        cursor="pointer"
+        fontSize="sm"
+        justify="center"
+        borderRadius="md"
+        borderWidth="1px"
+        _checked={{
+          bg: 'purple.100',
+          color: 'gray.700',
+          borderColor: 'purple.100',
+        }}
+        _focus={{
+          borderColor: 'purple.200',
+        }}
+      >
+        {props.children}
+      </Flex>
+    </Box>
+  )
+}
+
+interface Hash {
+  [key: string]: { [key: string]: AvailableSlot[] }
+}
+
+const TimesRadio = () => {
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'scheduledAt',
+    onChange: console.log,
+  })
+
+  // TODO: i'm using hard-coded `slots` here...
+  const slotsByVehicle = groupBy(slots, (item) => item['scheduledAt'])
+  const data = Object.entries(slotsByVehicle).reduce((hash, [k, v]) => {
+    hash[k] = groupBy(v, (item) => item['vehicleKey'])
+    return hash
+  }, {} as Hash)
+
+  const group = getRootProps()
+
+  return (
+    <VStack align="flex-start" spacing={8} {...group}>
+      {Object.entries(data).map(([date, slotsByVehicle], i) => {
+        return (
+          <Box key={i} w="100%">
+            <Text fontSize="sm" fontWeight="semibold" mb={4}>
+              {format(new Date(date), 'EEEE, MMMM d, yyyy')}
+            </Text>
+            <VStack
+              align="flex-start"
+              spacing={4}
+              w="100%"
+              divider={<StackDivider borderColor="gray.200" />}
+            >
+              {Object.entries(slotsByVehicle).map(
+                ([vehicleKey, timeSlots], j) => {
+                  return (
+                    <HStack key={`${i}-${j}`} spacing={4} w="100%">
+                      <Flex w="25%" fontSize="sm" color="gray.500">
+                        {timeSlots.length
+                          ? timeSlots[0]?.vehicleName
+                          : vehicleKey}
+                      </Flex>
+                      <SimpleGrid columns={4} spacing={2}>
+                        {timeSlots.map((time: AvailableSlot, k) => {
+                          const key = `${i}-${j}-${k}`
+                          const value = key + time.scheduledAt.toISOString()
+                          const radio = getRadioProps({ value })
+                          return (
+                            <TimeSlot key={key} {...radio}>
+                              {format(time.scheduledAtFull, 'hh:mm aa')}
+                            </TimeSlot>
+                          )
+                        })}
+                      </SimpleGrid>
+                    </HStack>
+                  )
+                }
+              )}
+            </VStack>
+          </Box>
+        )
+      })}
+    </VStack>
+  )
+}
 
 export interface TicketCreateModalProps {
   isOpen: boolean
@@ -43,10 +225,10 @@ export const TicketCreateModal = ({
     resolver: superstructResolver(TicketFormSchema, { coerce: true }),
   })
   const {
-    register,
+    //    register,
     handleSubmit,
     reset,
-    formState: { errors, isSubmitting },
+    //    formState: { errors, isSubmitting },
   } = form
   const handleFormSubmit: SubmitHandler<TicketInput> = (fields) => {
     // TODO: POST /api/tickets
@@ -57,10 +239,7 @@ export const TicketCreateModal = ({
 
   return (
     <Modal
-      preserveScrollBarGap
       size="xl"
-      isCentered
-      scrollBehavior="inside"
       isOpen={isOpen}
       onClose={() => {
         onClose()
@@ -148,172 +327,12 @@ export const TicketCreateModal = ({
                   borderRadius="md"
                   boxShadow="base"
                 >
-                  <VStack align="flex-start" spacing={8}>
-                    <Box w="100%">
-                      <Text fontSize="sm" fontWeight="semibold" mb={4}>
-                        Tuesday, August 19, 2021
-                      </Text>
-                      <VStack
-                        align="flex-start"
-                        spacing={4}
-                        w="100%"
-                        divider={<StackDivider borderColor="gray.200" />}
-                      >
-                        <HStack spacing={4} w="100%">
-                          <Flex w="25%" fontSize="sm" color="gray.500">
-                            Truck 102
-                          </Flex>
-                          <SimpleGrid columns={4} spacing={2}>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                          </SimpleGrid>
-                        </HStack>
-                        <HStack spacing={4} w="100%">
-                          <Flex w="25%" fontSize="sm" color="gray.500">
-                            Truck 202
-                          </Flex>
-                          <SimpleGrid columns={4} spacing={2}>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                          </SimpleGrid>
-                        </HStack>
-                      </VStack>
-                    </Box>
-                    <Box w="100%">
-                      <Text fontSize="sm" fontWeight="semibold" mb={4}>
-                        Wednesday, August 20, 2021
-                      </Text>
-                      <VStack
-                        align="flex-start"
-                        spacing={4}
-                        w="100%"
-                        divider={<StackDivider borderColor="gray.200" />}
-                      >
-                        <HStack spacing={4} w="100%">
-                          <Flex w="25%" fontSize="sm" color="gray.500">
-                            Truck 102
-                          </Flex>
-                          <SimpleGrid columns={4} spacing={2}>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              10:00am
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              12:00pm
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              1:00pm
-                            </Flex>
-                          </SimpleGrid>
-                        </HStack>
-                        <HStack spacing={4} w="100%">
-                          <Flex w="25%" fontSize="sm" color="gray.500">
-                            Truck 202
-                          </Flex>
-                          <SimpleGrid columns={4} spacing={2}>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                            <Flex
-                              fontSize="sm"
-                              borderRadius="md"
-                              borderWidth="1px"
-                              justify="center"
-                              p={2}
-                            >
-                              8:00am
-                            </Flex>
-                          </SimpleGrid>
-                        </HStack>
-                      </VStack>
-                    </Box>
-                  </VStack>
+                  <TimesRadio />
                 </Box>
               </VStack>
             </VStack>
 
+            {/*
             <FormControl isInvalid={!!errors.ticketKind} isRequired>
               <FormLabel htmlFor="ticketKind">Ticket type</FormLabel>
               <Select id="ticketKind" {...register('ticketKind')}>
@@ -346,6 +365,7 @@ export const TicketCreateModal = ({
             >
               Submit
             </Button>
+            */}
           </form>
         </ModalBody>
       </ModalContent>
