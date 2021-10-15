a7098501bf4a4ffc2860e24fc657befd281e3069
David Valles <davidtjvalles@gmail.com>
2021-10-15 13:12:40 -0700

    Configure Next to redirect root and admin routes

--- web/next.config.js
+++ web/next.config.js
@@ -2,4 +2,18 @@ module.exports = {
   eslint: {
     dirs: ['src'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/delivery',
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: '/admin/delivery',
+        permanent: true,
+      },
+    ]
+  },
 }
