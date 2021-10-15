f511bc30f1b585240112cc7afb3b1b8871076ec5
David Valles <davidtjvalles@gmail.com>
2021-10-15 13:11:24 -0700

    Remove color mode button

--- web/src/pages/admin/settings.tsx
+++ web/src/pages/admin/settings.tsx
@@ -2,12 +2,10 @@ import type { ReactElement } from 'react'
 import { Flex } from '@chakra-ui/react'
 import { AdminLayout } from '@/components/layouts/admin-layout'
 import { NotReadyYet } from '@/components/not-ready'
-import { ColorModeButtons } from '@/components/color-mode-buttons'
 
 const Settings = () => (
   <Flex direction="column" align="center" justify="center" h="100%">
     <NotReadyYet />
-    <ColorModeButtons />
   </Flex>
 )
 
